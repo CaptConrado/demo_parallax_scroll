d3a677b50f2970eb8da21b519653464b262c79d3
captconrado <ottey001@gmail.com>
2013-03-14 20:05:07 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParalaxApp::Application.config.secret_token = 'a57d465c1414c9376acbc5c6cb3f5f1ac7f61577fdd7756d5408083fe8df9242a0df47be04bdb62a4fea44a9dda22c05dde430455ff09a4835b572d763532877'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ParalaxApp::Application.config.session_store :cookie_store, key: '_paralax_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ParalaxApp::Application.config.session_store :active_record_store
